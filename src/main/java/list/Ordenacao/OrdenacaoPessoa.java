1d904d927bdf7acff878bf58f0bc7e5f681df615
monickecunha <monicke.oliveira@hotmail.com>
2025-07-22 13:58:02 -0300

    Adicionados exercícios ListaTarefa, CadastroProdutos e GerenciadorAlunos

---
+++ src/main/java/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,17 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+
+}
--- src/main/java/set/Pesquisa/AgendaContatos.java
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -37,6 +37,25 @@ public class AgendaContatos {
     }
 
     public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
 
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("João", 123456789);
+        agendaContatos.adicionarContato("Maria", 987654321);
+        agendaContatos.adicionarContato("Maria Fernandes", 55555555);
+        agendaContatos.adicionarContato("Ana", 88889999);
+        agendaContatos.adicionarContato("Fernando", 77778888);
+        agendaContatos.adicionarContato("Carolina", 55555555);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Maria"));
+
+        Contato contatoAtualizado = agendaContatos.atualizarNumeroContato("Carolina", 44443333);
+        System.out.println("Contato atualizado: " + contatoAtualizado);
+        
+        System.out.println("Contatos na agenda após atualização:");
+        agendaContatos.exibirContatos();
     }
 }
